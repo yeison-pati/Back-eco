fb2a5cb3fe54bfd3321bba1aa0464c2981bb58d0
ISA <isabelaurrego1117995@correo.itm.edu.co>
2025-04-16 18:27:50 -0500

    fix(controllers): agregads Pathvariable y @RequestBody

--- src/main/java/com/itm/ecosurprise/controllers/ComercianteController.java
+++ src/main/java/com/itm/ecosurprise/controllers/ComercianteController.java
@@ -1,9 +1,7 @@
 package com.itm.ecosurprise.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.itm.ecosurprise.models.Comerciante;
 import com.itm.ecosurprise.models.Consumidor;
@@ -18,13 +16,13 @@ public class ComercianteController {
 private ComercianteService comercianteService;
 
 @PostMapping("/crearComerciante")
-public Comerciante crearComerciante(Comerciante comerciante) {
+public Comerciante crearComerciante(@RequestBody Comerciante comerciante) {
     return comercianteService.crearComerciante(comerciante);
     
 }
 
-@PostMapping("/crearProducto")
-public Producto crearProducto(Comerciante idComerciante, Producto producto) {
+@PostMapping("/{id}/crearProducto")
+public Producto crearProducto(@PathVariable  Comerciante idComerciante, @RequestBody  Producto producto) {
     return comercianteService.crearProducto(idComerciante, producto);
 
 }
