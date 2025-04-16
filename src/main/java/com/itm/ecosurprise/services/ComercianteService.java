d597c4d9b0d4fb2db5cd816b93ba4748462bdacc
GeraldineTB07 <geraldinetorres1118266@correo.itm.edu.co>
2025-04-15 21:19:23 -0500

    add(services):creacion de algunos metodos para comerciante y producto

--- src/main/java/com/itm/ecosurprise/services/ComercianteService.java
+++ src/main/java/com/itm/ecosurprise/services/ComercianteService.java
@@ -1,5 +1,41 @@
 package com.itm.ecosurprise.services;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itm.ecosurprise.models.Comerciante;
+import com.itm.ecosurprise.models.Producto;
+import com.itm.ecosurprise.repositories.IComerciante;
 
 public class ComercianteService {
+
+
+@Autowired
+private IComerciante comercianteRepository;
+
+@Autowired
+private ProductoService productoService;
+
+public String saludar(){
+    return "Hola desde el servicio de Comerciante";
+}
+
+public Producto crearProducto(Long idComerciante, Producto producto){
+    producto.
+    return productoService.guardaProducto(producto);
+}
+
+
+
+public List<Comerciante> obtenerComeriantes(){
+    return comercianteRepository.findAll();
+}
+
+public Comerciante obtenerXID(Long id){
+    return comercianteRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Comerciante no encontrado con ID: " + id));
+}
+
+
 
 }
